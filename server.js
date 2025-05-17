6829515bd68347c24dfebc89b076ab5f2b48b532
mrdeepak125 <asus0gl8@outlook.com>
2025-05-17 18:36:00 +0530

    Update dependencies in package.json and modify Referer header in server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const headers = {
   'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
   'sec-ch-ua-mobile': '?0',
   'sec-ch-ua-platform': '"Windows"',
-  'Referer': 'https://multimovies.cloud/',
+  'Referer': 'https://multimovies.media/',
   'Sec-Fetch-User': '?1',
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
 };
